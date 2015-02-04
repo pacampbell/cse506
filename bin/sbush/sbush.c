79dff85d88f5e1bfeafee1f9667e31e979f2ecd1
Paul Campbell <paul.campbell@stonybrook.edu>
2015-02-04 16:33:20 -0500

    sbush
    Removed preprocessor defines from the source file and added them to the
    header file for the shell.

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define INPUT_BUFFER 1024
+#include "sbush.h"
 
 int main(int argc, char *argv[], char* envp[]) {
+    char buffer[INPUT_BUFFER];
     int running = 1;
     while(running) {
         printf("> ");
-        break;
-        //scanf()
+        scanf("%"XSTR(INPUT_BUFFER)"s", buffer);
     }
     return EXIT_SUCCESS;
 }
