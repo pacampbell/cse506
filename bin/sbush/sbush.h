2e26ccf437c254d5413103400ce4d6b5cc6b8cdc
Paul Campbell <paul.campbell@stonybrook.edu>
2015-02-04 16:32:49 -0500

    sbush.h
    Added a header file for the shell to the project.

---
+++ bin/sbush/sbush.h
@@ -0,0 +1,9 @@
+#ifndef _SBUSH_H
+#define _SBUSH_H
+
+// Stringification - https://gcc.gnu.org/onlinedocs/cpp/Stringification.html
+#define XSTR(A) STR(A)
+#define STR(A) #A
+#define INPUT_BUFFER 1024
+
+#endif
