c64b87e6b3c0ae803f332e34f96694a58dd22801
Paul Campbell <paul.campbell@stonybrook.edu>
2015-02-05 18:00:24 -0500

    Generic syscall interfaces and fixes
    Implemented syscall_0 - syscall_5. Fixed a bug in strlen.
    Added a wrapper for the write syscall and exit syscall.

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "sbush.h"
 
 int main(int argc, char *argv[], char* envp[]) {
     //char buffer[INPUT_BUFFER];
     int running = 1;
+    char *test = "Hello, World!\n";
+    size_t len = strlen(test);
     while(running && 0) {
         //printf("> ");
         //scanf("%"XSTR(INPUT_BUFFER)"s", buffer);
     }
-    putint(4);
+
+    write(1, test, len);
     return 1337;
 }
