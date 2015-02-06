8b29583dfdfa5280260643a0c25f350338804512
Paul Campbell <paul.campbell@stonybrook.edu>
2015-02-05 19:51:07 -0500

    Unused code
    Removed unused code.

--- include/string.h
+++ include/string.h
@@ -6,4 +6,5 @@ typedef uint64_t size_t;
 char *strcpy(char*d,const char*s);
 size_t strlen(const char*s);
 int strcmp(const char *s1, const char *s2);
+
 #endif
--- libc/stdlib.c
+++ libc/stdlib.c
@@ -5,13 +5,6 @@ void exit(int status) {
     syscall_1(SYS_exit, status);
 }
 
-void putint(int value) {
-    char *test = "Test\n";
-    // syscall_3()
-    // syscall_3(SYS_write, 1, "Test\n", 5);
-    syscall_3(SYS_write, 1, (uint64_t)test, 5);
-}
-
 ssize_t write(int fd, const void *buf, size_t count) {
     return syscall_3(SYS_write, fd, (uint64_t)buf, count);
 }
