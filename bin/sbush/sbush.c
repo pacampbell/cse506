d9d6c85f2b609a98436211d2344aeff374fb673c
Paul Campbell <paul.campbell@stonybrook.edu>
2015-02-12 18:06:36 -0500

    sbush
    Lots of debug tests.

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
 #include "sbush.h"
-
-#define MAX_ARGS 1
-
-char *find_env_var(char* envp[], char* name);
 
 int test_main(int argc, char *argv[], char* envp[]);
 int sbsh_main(int argc, char* argv[], char* envp[]);
@@ -34,66 +26,54 @@ int test_main(int argc, char *argv[], char* envp[]) {
 }
 
 int sbsh_main(int argc, char* argv[], char* envp[]) {
-    char cmd[256] = {0};
-    char *name[] = {"fake", NULL};
-    char *path;
-    char *ps1;
-    int rc;
-    char *c;
+    // char cmd[256] = {0};
+    char *cmd = "ls -l | grep crt | echo hello";
+    // char *name[] = {"fake", NULL};
+    char *path = NULL;
+    // char *ps1;
+    int rc = 0;
+    char *c = NULL;
 
-    char *str = malloc(8);
-    char *str2 = malloc(50);
-    char *str3 = malloc(80);
-    free(str2);
-    str2 = malloc(10);
-    // Use the strings
-    str[0] = 'H';
-    str[1] = 'e';
-    str[2] = 'l';
-    str[3] = 'l';
-    str[4] = 'o';
-    str[5] = '\n';
+    // ps1 = find_env_var(envp, "PS1");
 
-    str2[0] = 'S';
-    str2[1] = '\0';
-
-    str3[0] = 'Q';
-    str3[1] = '\0';
-
-    write(STDOUT_FILENO, str, strlen(str));
-    write(STDOUT_FILENO, str2, strlen(str2));
-    write(STDOUT_FILENO, str3, strlen(str3));
-
-    //char *str = malloc(8);
-    //str[0] = '\0';
-
-    ps1 = find_env_var(envp, "PS1");
-
+    /*
     for(c = *envp, rc = 0; c != NULL; rc++, c = *(envp + rc)) {
         printf("ENV::: %s\n", c);
     }
+    */
 
+    /*
     if(ps1 == NULL) {
         printf("> ");
         read(STDIN_FILENO, cmd, 256);
     } else {
         printf("%s", ps1);
     }
+    */
 
-    c = cmd;
-    for(;*c != '\n' && *c != 0; c++);
-    *c = 0;
+    printf("> %s\n", cmd);
+
+    char **parsed_command = NULL; // parse_cmd(cmd);
+    char ***commands = extract_commands(cmd);
+    while(*commands != NULL) {
+        // Get the current command in the interation
+        parsed_command = *commands;
+        for(int i = 0; parsed_command[i] != NULL; i++) {
+            printf("cmd> %s\n", parsed_command[i]);
+        }
+        // Get the next command in the list
+        commands++;
+    }
 
     //find the path var
     path = find_env_var(envp, "PATH");
 
     //printf("path: %s\n", path);
-
-    rc = execve(cmd, name, envp);
+    rc = execve(parsed_command[0], parsed_command, envp);
     c = strtok(path, ':');
     while(c != NULL) {
         //printf("trying: %s\n", strappend(c, "/", cmd));
-        rc = execve(strappend(c, "/", cmd), name, envp);
+        rc = execve(strappend(c, "/", parsed_command[0]), parsed_command, envp);
         c = strtok(NULL, ':');
     }
 
@@ -114,82 +94,3 @@ char *find_env_var(char* envp[], char* name) {
 
     return var;
 }
-
-/*
-   void evaluateCommand(char **cmd, int cmdSize, int *running, char* wd, char** envp, int debug, char *historyList[], int rdSize) {
-   char *arguments[MAX_ARGS];
-
-// Something went wrong stop evaluating.
-if(cmdSize <= 0){
-return;
-}
-// Check to see how many commands we need to evaluate
-if(!rdSize){
-if (strlen(*cmd)) {
-if (debug) {
-printf("RUNNING:%s\n", *cmd);
-}
-parseCommand(*cmd, arguments, MAX_ARGS);
-if (!strcmp(arguments[0], "exit")) {
- *running = false;
-
- } else if (!strcmp(arguments[0], "cd")) {
-
-
- if (arguments[1] == NULL || !strcmp(arguments[1], "~")) {
- setenv("OLDPWD", wd, 1);
- if (debug) printf("oldpwd: %s\n", getenv("OLDPWD"));
- chdir(parseEnv(envp, "HOME"));
-
- } else if (!strcmp(arguments[1], "-")) {
-                    chdir(getenv("OLDPWD"));
-                } else {
-                    setenv("OLDPWD", wd, 1);
-                    if (debug) printf("oldpwd: %s\n", getenv("OLDPWD"));
-                    int val = chdir(arguments[1]);
-                    if (val) printf("Sorry but %s does not exist\n", arguments[1]);
-                }
-
-            } else if (!strcmp(arguments[0], "set")) {
-                setenv(arguments[1], arguments[3], 1);
-
-            } else if (!strcmp(arguments[0], "echo")) {
-                int index = contains(arguments, '$');
-                char *cp = NULL;
-                if(index > -1) cp = arguments[index];
-                if (cp != NULL) {
-                    cp++;
-                    printf("%s\n", getenv(cp));
-                } else {
-                    spawn(arguments);
-                }
-
-            } else if(!strcmp(arguments[0], "history")) {
-                for (int i = 0; i < MAX_HISTORY && *historyList[i] != '\0'; ++i) {
-                    printf("%s\n", historyList[i]);
-                }
-            } else if(!strcmp(arguments[0], "wolfie")){
-                printWolf();
-            } else if(!strcmp(arguments[0], "cls")){
-                strcpy(arguments[0], "clear");
-                spawn(arguments);
-            } else if(!strcmp(arguments[0], "clear")){
-                for (int i = 0; i < MAX_HISTORY; ++i) {
-                    *historyList[i] = '\0';
-                }
-            } else if(!strcmp(arguments[0], "clear")){
-                for (int i = 0; i < MAX_HISTORY; ++i) {
-                    *historyList[i] = '\0';
-                }
-            } else {
-                spawn(arguments);
-            }
-            if (debug) {
-                printf("ENDED: %s (needs return val)\n", *cmd);
-            }
-        }
-    } else {
-        spawnRedirect(cmd, cmdSize, redirects, rdSize);
-    }
-
-}*/
--- bin/sbush/sbush.h
+++ bin/sbush/sbush.h
@@ -1,5 +1,14 @@
 #ifndef _SBUSH_H
 #define _SBUSH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "cmdparse.h"
+
+#define MAX_ARGS 1
+
+char *find_env_var(char* envp[], char* name);
 
 // Stringification - https://gcc.gnu.org/onlinedocs/cpp/Stringification.html
 #define XSTR(A) STR(A)
