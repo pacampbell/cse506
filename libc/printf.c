5c533db124317b507bbd4a0e8bbf753ae5ef198a
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-02-14 16:16:15 -0500

    added back  old printf

--- libc/printf.c
+++ libc/printf.c
@@ -83,8 +83,61 @@ int fprintf(int fd, const char *format, ...) {
 }
 
 int printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    return fprintf(STDOUT_FILENO, format, args);
+    va_list val;
+    int printed = 0;
+    // Temps used for printing
+    int tmp_int = 0;
+    unsigned int tmp_hex = 0;
+    char *tmp_cp;
+    // Start getting values for varargs
+    va_start(val, format);
+    // Loop through the format string
+    while(*format) {
+        // Look for % sign
+        if(*format == '%') {
+            switch(*(format + 1)) {
+                case '%':
+                    write(STDOUT_FILENO, "%", sizeof(char));
+                    printed += 1;
+                    break;
+                case 's':
+                    tmp_cp = va_arg(val, char*);
+                    write(STDOUT_FILENO, tmp_cp, strlen(tmp_cp));
+                    printed += strlen(tmp_cp);
+                    break;
+                case 'd':
+                    tmp_int = va_arg(val, int);
+                    if(tmp_int < 0) {
+                        // Write out the negative sign
+                        write(STDOUT_FILENO, "-", sizeof(char));
+                        // Get the absolute value of d; invert bits and add 1
+                        tmp_int = ~tmp_int + 1;
+                    }
+                    // Convert the number to decimal
+                    print_base(STDOUT_FILENO, tmp_int, BASE_10, &printed, 0);
+                    break;
+                case 'x':
+                    tmp_hex = va_arg(val, unsigned int);
+                    // Convert the number to hex
+                    print_base(STDOUT_FILENO, tmp_hex, BASE_16, &printed, 0);
+                    break;
+                case 'c':
+                    tmp_int = va_arg(val, int);
+                    write(STDOUT_FILENO, &tmp_int, sizeof(char));
+                    break;
+                default:
+                    printf("\n-----\nyou broke printf\n-----\n");
+                    return -1;
+                    break;
+            }
+            // Increment onto the format character
+            ++format;
+        } else {
+            write(STDOUT_FILENO, format, sizeof(char));
+            printed++;
+        }
+        ++format;
+    }
+    return printed;
 }
 
