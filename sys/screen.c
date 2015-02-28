033ff0f3a857c2cc861f85df1b5253d3f680d170
Paul Campbell <paul.campbell@stonybrook.edu>
2015-02-27 22:02:26 -0500

    screen
    Added support for '\b' and '\r'

--- sys/screen.c
+++ sys/screen.c
@@ -15,18 +15,25 @@ void putk(char c) {
 }
 
 void putck(char color, char c) {
-    if(c != '\n') {
+    if(c != '\n' && c != '\r' && c != '\b') {
         // Write the character
         *(VIDEO_MEM + (cursor_y * (TERMINAL_COLUMNS * 2)) + cursor_x) = c;
         // Write the color information
         *(VIDEO_MEM + (cursor_y * (TERMINAL_COLUMNS * 2)) + cursor_x + 1) = color;
         // Increment the current cursor by 1 whole position (2-bytes)
         cursor_x += 2;
-        // Check to see if we should increment by column
     }
+    // Begin checking for special situations
     if(c == '\n' || cursor_x == (TERMINAL_COLUMNS * 2)) {
+        // Increment to the next row and reset the column
         cursor_x = 0;
         cursor_y += 1;
+    } else if(c == '\r') {
+        // Set the cursor back to the start of the current row
+        cursor_x = 0;
+    } else if(c == '\b') {
+        // Go back by 1 character in the current row
+        cursor_x = cursor_x - 2 < 0 ? 0 : cursor_x - 2;
     }
 }
 
