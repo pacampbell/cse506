91214bdbba1c751dbbba1b6de7832d57a727466c
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-02-28 19:54:11 -0500

    removed the broken print function

--- sys/printk.c
+++ sys/printk.c
@@ -34,19 +34,6 @@ size_t strlen(const char *s) {
     return length;
 }
 
-
-void printb(const char *fmt, ...) {
-    /* Currently does nothing */
-    volatile char *video_memory = (volatile char*)0xb8000;
-    while(*fmt != '\0') {
-        *video_memory = *fmt++;
-        // 2 bytes for each output
-        // |    Ascii Code   | Color Information |
-        // |    1-byte       |   1-byte          |
-        video_memory += 2;
-    }
-}
-
 void printk(const char *format, ...) {
     va_list val;
     int printed = 0;
