a3b9b3f655796d5500451cae0693ff4311b40ab0
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-04-12 17:49:08 -0400

    added freelist

--- include/sbunix/pgtable.h
+++ include/sbunix/pgtable.h
@@ -4,6 +4,14 @@
     #ifndef _PGTABLE_H
     #define _PGTABLE_H
         #include <sys/defs.h>
+        
+        #define MAX_PAGES 8192
+        #define WORD_SIZE 32
+        #define PAGE_SIZE 0x1000
+
+        void init_free_pg_list(void *physfree);
+        uint32_t is_pg_free(int page);
+        uint32_t set_pg_free(int page, int free);
 
         void initializePaging(void);
         void loadPageDirectory(uint64_t *address);
--- sys/pgtable.c
+++ sys/pgtable.c
@@ -7,12 +7,55 @@ uint64_t page_dir_ptr_tab[4] __attribute__((aligned(0x20)));
 uint64_t page_dir[512] __attribute__((aligned(0x1000)));  // must be aligned to page boundary
 uint64_t page_tab[512] __attribute__((aligned(0x1000)));
 
+uint32_t *free_pg_list;
+void* free_pg_list_end;
+
+/**
+ * physfree: starts the free list at this location
+ */
+void init_free_pg_list(void *physfree) {
+    free_pg_list = (uint32_t *) physfree;
+
+    //get how many words needed for bitmap
+    int size = (MAX_PAGES / WORD_SIZE);
+    uint32_t all_on = 0xFFFFFFFF;
+
+    for(int i = 0; i < size; i++) {
+        *(free_pg_list + i) |= all_on;
+    }
+
+    free_pg_list_end = free_pg_list + size;
+}
+
+/**
+ * page: the page cunt not the page address
+ */
+uint32_t is_pg_free(int page) {
+    int word_num = (page / WORD_SIZE);
+    return free_pg_list[word_num] & ~(1 << (page / WORD_SIZE));
+}
+
+/**
+ * page: count of the page to set
+ * free: 0 will mark it not free, anything else sets it free
+ */
+uint32_t set_pg_free(int page, int free) {
+    int word_num = (page / WORD_SIZE);
+    if(free) {
+        free_pg_list[word_num] |= (1 << (page / WORD_SIZE));
+    } else {
+        free_pg_list[word_num] &= ~(1 << (page / WORD_SIZE));
+    }
+
+    return free_pg_list[word_num];
+}
+
 void initializePaging(void) {
     /**
-    +-----------------+-------+------+---+---+------+-----+-----+---+
-    | Frame Address   | AVAIL | RSVD | D | A | RSVD | U/S | R/W | P |
-    +-----------------+-------+------+---+---+------+-----+-----+---+
-    */
+      +-----------------+-------+------+---+---+------+-----+-----+---+
+      | Frame Address   | AVAIL | RSVD | D | A | RSVD | U/S | R/W | P |
+      +-----------------+-------+------+---+---+------+-----+-----+---+
+      */
 
     // set the page directory into the PDPT and mark it present
     page_dir_ptr_tab[0] = (uint64_t)&page_dir | 1;
@@ -27,54 +70,54 @@ void initializePaging(void) {
     // Call functions to set up
     disablePaging();
     // enablepae();
-	// loadPageDirectory(page_dir_ptr_tab);
-	// enablePaging();
+    // loadPageDirectory(page_dir_ptr_tab);
+    // enablePaging();
 }
 
 void disablePaging(void) {
     uint64_t rv = 0;
     __asm__ __volatile__ (
-        // "movq $0x1, %%rax;"
-        // "cpuid;"
-        // "movq %%rax, %0;"
-        "movq $0xC0000080, %%rcx;"
-        "rdmsr;"
-        "movq %%rax, %0;"
-        : "=r"(rv)
-        :
-        : "%rax", "%rcx"
-    );
+            // "movq $0x1, %%rax;"
+            // "cpuid;"
+            // "movq %%rax, %0;"
+            "movq $0xC0000080, %%rcx;"
+            "rdmsr;"
+            "movq %%rax, %0;"
+            : "=r"(rv)
+            :
+            : "%rax", "%rcx"
+            );
 
     printk("msr: 0x%x\n", rv);
 }
 
 void loadPageDirectory(uint64_t *address) {
     __asm__ __volatile__ (
-        "movq %0, %%cr3"
-        :
-        : "r" (&page_dir_ptr_tab)
-    );
+            "movq %0, %%cr3"
+            :
+            : "r" (&page_dir_ptr_tab)
+            );
 }
 
 void enablepae(void) {
     __asm__ __volatile__(
-        "movq %%cr4, %%rax;"
-        "bts $5, %%rax;"
-        "movq %%rax, %%cr4;"
-        :
-        :
-        : "%rax"
-    );
+            "movq %%cr4, %%rax;"
+            "bts $5, %%rax;"
+            "movq %%rax, %%cr4;"
+            :
+            :
+            : "%rax"
+            );
 }
 
 void enablePaging(void) {
     __asm__ __volatile__(
-        "movq %%cr0, %%rax;"
-        "movq $0x80000000, %%rdx;"
-        "orq %%rdx, %%rax;"
-        "movq %%rax, %%cr0;"
-        :
-        :
-        : "%rax", "%rdx"
-    );
+            "movq %%cr0, %%rax;"
+            "movq $0x80000000, %%rdx;"
+            "orq %%rdx, %%rax;"
+            "movq %%rax, %%cr0;"
+            :
+            :
+            : "%rax", "%rdx"
+            );
 }
