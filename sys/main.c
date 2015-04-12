671cc49fac026c68ba8daeb8bb992c54c0267bf2
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-04-12 14:27:12 -0400

    fixed indent

--- sys/main.c
+++ sys/main.c
@@ -9,19 +9,19 @@
 #include <sbunix/pgtable.h>
 
 void start(uint32_t* modulep, void* physbase, void* physfree) {
-	struct smap_t {
-		uint64_t base, length;
-		uint32_t type;
-	}__attribute__((packed)) *smap;
-	while(modulep[0] != 0x9001) modulep += modulep[1]+2;
-	for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
-		if (smap->type == 1 /* memory */ && smap->length != 0) {
-			printk("Available Physical Memory [%x-%x]\n", smap->base, smap->base + smap->length);
-		}
-	}
-	printk("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
-	// Setup paging
-	initializePaging();
+    struct smap_t {
+        uint64_t base, length;
+        uint32_t type;
+    }__attribute__((packed)) *smap;
+    while(modulep[0] != 0x9001) modulep += modulep[1]+2;
+    for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
+        if (smap->type == 1 /* memory */ && smap->length != 0) {
+            printk("Available Physical Memory [%x-%x]\n", smap->base, smap->base + smap->length);
+        }
+    }
+    printk("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
+    // Setup paging
+    initializePaging();
 }
 
 #define INITIAL_STACK_SIZE 4096
@@ -31,25 +31,25 @@ extern char kernmem, physbase;
 struct tss_t tss;
 
 void boot(void) {
-	// note: function changes rsp, local stack variables can't be practically used
-	// register char *s, *v;
-	__asm__(
-		"movq %%rsp, %0;"
-		"movq %1, %%rsp;"
-		:"=g"(loader_stack)
-		:"r"(&stack[INITIAL_STACK_SIZE])
-	);
-	// Initialize the descript tables and tss
-	reload_gdt();
-	setup_tss();
-	init_idt();
-	init_timer(50);
+    // note: function changes rsp, local stack variables can't be practically used
+    // register char *s, *v;
+    __asm__(
+            "movq %%rsp, %0;"
+            "movq %1, %%rsp;"
+            :"=g"(loader_stack)
+            :"r"(&stack[INITIAL_STACK_SIZE])
+           );
+    // Initialize the descript tables and tss
+    reload_gdt();
+    setup_tss();
+    init_idt();
+    init_timer(50);
     init_keyboard();
-     __asm("sti");
-	start(
-		(uint32_t*)((char*)(uint64_t)loader_stack[3] + (uint64_t)&kernmem - (uint64_t)&physbase),
-		&physbase,
-		(void*)(uint64_t)loader_stack[4]
-	);
-	while(1);
+    __asm("sti");
+    start(
+            (uint32_t*)((char*)(uint64_t)loader_stack[3] + (uint64_t)&kernmem - (uint64_t)&physbase),
+            &physbase,
+            (void*)(uint64_t)loader_stack[4]
+         );
+    while(1);
 }
