29b6267b372eb1ee5e41c6484955fe919331944d
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-04-16 17:07:21 -0400

    added the memory print again

--- sys/main.c
+++ sys/main.c
@@ -25,7 +25,7 @@ void start(uint32_t* modulep, void* physbase, void* physfree) {
     while(modulep[0] != 0x9001) modulep += modulep[1]+2;
     for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
         if (smap->type == 1 /* memory */ && smap->length != 0) {
-            // printk("Available Physical Memory [%x-%x]\n", smap->base, smap->base + smap->length);
+            printk("Available Physical Memory [%x-%x]\n", smap->base, smap->base + smap->length);
             set_kern_pg_used(smap->base,smap->base + smap->length);
         }
     }
