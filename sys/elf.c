49815d36f0d7ae31607736a012b3f7334e9c8ee6
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-04-29 23:19:28 -0400

    finally found the addresses of txt section

--- sys/elf.c
+++ sys/elf.c
@@ -1,6 +1,8 @@
 #define __KERNEL__
 #include <sys/elf.h>
+#include <sbunix/string.h>
 
+void print_elf_hdr(Elf64_Ehdr *hdr);
 
 void load_elf(char *data, uint64_t length) {
     if(validate_header(data)) {
@@ -9,18 +11,61 @@ void load_elf(char *data, uint64_t length) {
         Elf64_Shdr *section = (Elf64_Shdr*)(hdr->e_shoff + (char*)data);
 
         if(hdr->e_shstrndx == 0x00) panic("NO STRING TABLE");
-        char* str_tab =((char*)data + section[hdr->e_shstrndx].sh_offset);
+        char* str_tab = ((char*)data + section[hdr->e_shstrndx].sh_offset);
         //char* str_tab = (char*)(section[(hdr->e_shstrndx )]);
         int num_secs = hdr->e_shnum;
-
+        char *name;//, *txt;
         for(int i = 0; i < num_secs; i++) {
-            printk("name: %s\n", &str_tab[section[i].sh_name]);
-            section = section + section->sh_size;
+            name = &str_tab[section[i].sh_name];
+            printk("name: %s\n", name);
+            
+            if(strcmp(".text", name)) {
+                //txt = ((char*)data + section[i].sh_offset);
+                printk("found .txt at: %p\n", section[i].sh_addr);
+
+            } else if(strcmp(".rodata", name)) {
+                printk("found .rodata at: %p\n", section[i].sh_addr);
+
+            } else if(strcmp(".data", name)) {
+                printk("found .data at: %p\n", section[i].sh_addr);
+
+            }
         }
 
     } else {
         printk("Invalid ELF header for system.\n");
     }
+}
+
+
+void print_elf_hdr(Elf64_Ehdr *hdr) {
+    printk("hdr->e_type      %p\n", hdr->e_type);    
+    printk("hdr->e_entry     %p\n", hdr->e_entry);   
+    printk("hdr->e_flags     %p\n", hdr->e_flags);   
+    printk("hdr->e_ident     %p\n", hdr->e_ident);   
+    printk("hdr->e_phnum     %p\n", hdr->e_phnum);   
+    printk("hdr->e_phoff     %p\n", hdr->e_phoff);   
+    printk("hdr->e_shnum     %p\n", hdr->e_shnum);   
+    printk("hdr->e_shoff     %p\n", hdr->e_shoff);   
+    printk("hdr->e_ehsize    %p\n", hdr->e_ehsize);  
+    printk("hdr->e_machine   %p\n", hdr->e_machine); 
+    printk("hdr->e_version   %p\n", hdr->e_version); 
+    printk("hdr->e_shstrndx  %p\n", hdr->e_shstrndx);
+    printk("hdr->e_shentsize %p\n", hdr->e_shentsize);
+    printk("hdr->e_phentsize %p\n", hdr->e_phentsize);
+}
+
+void print_elf_sec(Elf64_Shdr *section) {
+    printk("section->sh_addr      %p\n", section->sh_addr);
+    printk("section->sh_info      %p\n", section->sh_info);
+    printk("section->sh_link      %p\n", section->sh_link);
+    printk("section->sh_name      %p\n", section->sh_name);
+    printk("section->sh_size      %p\n", section->sh_size);
+    printk("section->sh_type      %p\n", section->sh_type);
+    printk("section->sh_flags     %p\n", section->sh_flags);
+    printk("section->sh_offset    %p\n", section->sh_offset);
+    printk("section->sh_entsize   %p\n", section->sh_entsize);
+    printk("section->sh_addralign %p\n", section->sh_addralign);
 }
 
 
