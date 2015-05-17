56167b67c54d254896ce3cbff7f4deb85d655421
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-05-17 15:55:38 -0400

    fixed mt askexec thing

--- bin/askexec/askexec.c
+++ bin/askexec/askexec.c
@@ -8,25 +8,19 @@ int main(int argc, char *argv[], char *envp[]) {
     char c[BUFFER_SIZE];
     //pid_t pid;
     while(1) {
+        //pid_t pid;
     	memset(c, 0, BUFFER_SIZE);
         printf("Enter a file, no more than %d chars: ", BUFFER_SIZE);
+        int i = read(0, c, BUFFER_SIZE);
+        if(i < 1) continue;
         printf("running: %s\n", c);
         //pid = fork();
         //if(pid == 0) {
-        pid_t pid;
 
-        switch((pid = fork())) {
-        	case 0:
+            //execve(c, argv, envp);
             execve(c, argv, envp);
-            //execve("bin/args", argv, envp);
             printf("that was a bad program name, try again\n");
             return 0;
         //}
-        		printf("Failed to fork.\n");
-        		break;
-        	default:
-        		/* just continue */
-        		break;
-        } 
     }
 }
