b58e2c63906bbefb8411af1eb43474f42985f0a5
Michael Salerno <michael.j.salerno@stonybrook.edu>
2015-05-17 15:08:52 -0400

    added fork commented out in askexec

--- bin/askexec/askexec.c
+++ bin/askexec/askexec.c
@@ -3,11 +3,17 @@
 
 int main(int argc, char *argv[], char *envp[]) {
     char c[20];
+    //pid_t pid;
     while(1) {
         printf("Enter a file, no more than 19 chars: ");
         read(0, c, 20);
-        printf("execing: %s\n", c);
-        execve(c, argv, envp);
-        printf("that was a bad program name, try again\n");
+        printf("running: %s\n", c);
+        //pid = fork();
+        //if(pid == 0) {
+            execve(c, argv, envp);
+            //execve("bin/args", argv, envp);
+            printf("that was a bad program name, try again\n");
+            return 0;
+        //}
     }
 }
